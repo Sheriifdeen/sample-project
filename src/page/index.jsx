2e09a3b3c24ac889b8994d77f9fe9662d94d48aa
Sheriifdeen <sherifdheen@gmail.com>
2024-11-22 00:03:21 +0100

    Initial commit

---
+++ src/page/index.jsx
@@ -0,0 +1,276 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='rectangle'>
+        <span className='landie'>Landing</span>
+        <div className='bg'>
+          <span className='button-label'>Buy Now</span>
+        </div>
+        <span className='contact'>Contact</span>
+        <span className='home'>Home</span>
+        <span className='about'>About</span>
+      </div>
+      <div className='undraw-mobile-login'>
+        <div className='mask' />
+        <div className='undraw-mobile-login-1' />
+        <div className='text-block-section'>
+          <span className='title'>Light, Fast & Powerful</span>
+          <span className='body'>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus <br />
+            mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
+            sem. Nulla consequat massa quis enim.
+          </span>
+        </div>
+        <div className='element-card-info'>
+          <div className='icon-position-left'>
+            <div className='icon'>
+              <div className='combined-shape' />
+            </div>
+          </div>
+          <span className='title-goes-here'>Title Goes Here</span>
+          <span className='body-2'>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor.
+          </span>
+        </div>
+        <div className='element-card-info-3'>
+          <div className='icon-position-left-4'>
+            <div className='icon-5'>
+              <div className='combined-shape-6' />
+            </div>
+          </div>
+          <span className='title-goes-here-7'>Title Goes Here</span>
+          <span className='body-8'>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor.
+          </span>
+        </div>
+      </div>
+      <div className='rectangle-9'>
+        <div className='undraw-happy-news' />
+        <div className='flex-column-d'>
+          <span className='title-a'>Light, Fast & Powerful</span>
+          <span className='body-b'>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus <br />
+            mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
+            sem. Nulla consequat massa quis enim.
+          </span>
+        </div>
+      </div>
+      <div className='rectangle-c'>
+        <div className='rectangle-d'>
+          <div className='path'>
+            <div className='rectangle-e'>
+              <div className='path-f'>
+                <div className='flex-column'>
+                  <div className='oval'>
+                    <div className='oval-10' />
+                  </div>
+                  <div className='path-11' />
+                </div>
+                <div className='path-12' />
+              </div>
+              <div className='path-13'>
+                <div className='rectangle-14' />
+                <div className='oval-15' />
+              </div>
+              <div className='path-16' />
+              <div className='path-17' />
+              <div className='path-18' />
+              <div className='path-19'>
+                <div className='path-1a' />
+              </div>
+            </div>
+            <div className='rectangle-1b' />
+            <div className='rectangle-1c' />
+            <div className='oval-1d' />
+            <div className='rectangle-1e' />
+            <div className='oval-1f' />
+            <div className='rectangle-20' />
+            <div className='path-21' />
+            <div className='path-22' />
+            <div className='path-23' />
+          </div>
+        </div>
+        <div className='rectangle-24' />
+        <div className='path-25'>
+          <div className='path-26' />
+        </div>
+        <div className='path-27' />
+        <div className='rectangle-28' />
+        <span className='title-29'>Light, Fast & Powerful</span>
+        <div className='rectangle-2a' />
+        <div className='rectangle-2b' />
+        <div className='path-2c' />
+        <div className='path-2d' />
+        <span className='body-2e'>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus <br />
+          mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
+          sem. Nulla consequat massa quis enim.
+        </span>
+        <div className='path-2f'>
+          <div className='path-30' />
+          <div className='oval-31' />
+        </div>
+        <div className='path-32'>
+          <div className='group' />
+          <div className='oval-33' />
+          <div className='path-34' />
+        </div>
+        <div className='path-35' />
+        <div className='path-36'>
+          <div className='group-37' />
+        </div>
+        <div className='path-38'>
+          <div className='path-39'>
+            <div className='path-3a' />
+          </div>
+        </div>
+        <div className='path-3b' />
+        <div className='path-3c' />
+        <div className='path-3d'>
+          <div className='path-3e' />
+        </div>
+        <div className='path-3f' />
+        <div className='path-40' />
+        <div className='path-41' />
+        <div className='path-42' />
+        <div className='path-43'>
+          <div className='path-44' />
+        </div>
+        <div className='path-45'>
+          <div className='path-46' />
+        </div>
+        <div className='path-47' />
+        <div className='path-48' />
+        <div className='path-49' />
+        <div className='path-4a' />
+      </div>
+      <div className='rectangle-4b'>
+        <div className='undraw-mention-kd' />
+        <div className='flex-column-d-4c'>
+          <span className='title-4d'>Light, Fast & Powerful</span>
+          <span className='body-4e'>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus <br />
+            mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
+            sem. Nulla consequat massa quis enim.
+          </span>
+          <div className='bg-4f'>
+            <span className='button-label-50'>Purchase Now</span>
+          </div>
+        </div>
+      </div>
+      <div className='path-51' />
+      <div className='mask-52'>
+        <span className='title-53'>
+          Introduce Your Product Quickly & Effectively
+        </span>
+        <span className='body-54'>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus <br />
+          mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
+          sem. Nulla consequat massa quis enim.
+        </span>
+        <div className='flex-row-a'>
+          <div className='bg-55'>
+            <span className='purchase-ui-kit'>Purchase UI Kit</span>
+          </div>
+          <div className='bg-56'>
+            <span className='learn-more'>Learn More</span>
+          </div>
+        </div>
+      </div>
+      <div className='group-57'>
+        <div className='rectangle-58'>
+          <div className='rectangle-59'>
+            <div className='rectangle-5a' />
+            <div className='rectangle-5b' />
+            <div className='rectangle-5c' />
+            <div className='rectangle-5d' />
+            <div className='rectangle-5e' />
+            <div className='rectangle-5f' />
+            <div className='rectangle-60' />
+            <div className='rectangle-61' />
+            <div className='rectangle-62' />
+            <div className='rectangle-63' />
+            <div className='rectangle-64' />
+            <div className='rectangle-65' />
+            <div className='rectangle-66' />
+            <div className='rectangle-67' />
+            <div className='rectangle-68' />
+            <div className='rectangle-69' />
+            <div className='rectangle-6a' />
+            <div className='rectangle-6b' />
+            <div className='rectangle-6c' />
+            <div className='rectangle-6d' />
+            <div className='rectangle-6e' />
+            <div className='rectangle-6f' />
+            <div className='rectangle-70' />
+            <div className='rectangle-71' />
+            <div className='rectangle-72' />
+            <div className='rectangle-73' />
+            <div className='rectangle-74' />
+            <div className='rectangle-75' />
+            <div className='rectangle-76' />
+            <div className='rectangle-77' />
+            <div className='rectangle-78' />
+            <div className='rectangle-79' />
+            <div className='rectangle-7a' />
+            <div className='rectangle-7b' />
+            <div className='rectangle-7c' />
+            <div className='rectangle-7d' />
+            <div className='rectangle-7e' />
+            <div className='rectangle-7f' />
+            <div className='rectangle-80' />
+            <div className='rectangle-81' />
+          </div>
+        </div>
+      </div>
+      <div className='rectangle-82' />
+      <span className='title-83'>A Price To Suit Everyone</span>
+      <span className='body-84'>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus
+      </span>
+      <span className='price'>$40</span>
+      <span className='title-85'>UI Design Kit</span>
+      <span className='body-86'>See, One price. Simple.</span>
+      <div className='bg-87'>
+        <span className='button-label-88'>Purchase Now</span>
+      </div>
+      <div className='rectangle-89'>
+        <div className='flex-row-dce'>
+          <span className='landie-8a'>Landing</span>
+          <div className='bg-8b'>
+            <span className='button-label-8c'>Purchase now</span>
+          </div>
+          <span className='copyright'>©2023 Yourcompany</span>
+        </div>
+        <div className='rectangle-8d' />
+        <div className='flex-row-b'>
+          <span className='home-8e'>Home</span>
+          <span className='about-8f'>About</span>
+          <span className='contact-90'>Contact</span>
+          <div className='path-91' />
+          <div className='combined-shape-92' />
+          <div className='combined-shape-93' />
+          <div className='path-94' />
+          <div className='shape' />
+        </div>
+      </div>
+    </div>
+  );
+}
